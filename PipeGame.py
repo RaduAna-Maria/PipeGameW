50cf0a7a5515c853ba92c79845ef334462678e64
Ana-MariaNae <ana_maria.nae@stud.acs.upb.ro>
2022-05-12 23:47:33 +0300

    moving the square

--- PipeGame.py
+++ PipeGame.py
@@ -21,94 +21,141 @@ y_start = 72
 grid_width = 10 * square_lat
 grid_height = 7 * square_lat
 
+#types of pipes
+# 0 = corner
+# 1 = tube
+pipe_type = [[0, 1, 0, 1, 1, 0, 1, 0, 1, 0],
+             [1, 0, 0, 0, 1, 0, 1, 1, 0, 1],
+             [0, 1, 1, 1, 0, 1, 0, 1, 1, 0],
+             [1, 1, 0, 0, 1, 1, 1, 1, 0, 1],
+             [1, 0, 1, 0, 0, 0, 0, 0, 1, 1],
+             [1, 1, 0, 1, 0, 1, 1, 1, 1, 1],
+             [1, 0, 0, 0, 1, 1, 1, 0, 1, 0]]
+
+tube_directions = {0: "up-down", 1:"left-right"}
+corner_directions = {0: "up-right", 1:"right-down", 2:"down-left", 3:"left-up"}
 
 class square: 
-    def __init__(self, game, x, y):
+    def __init__(self, game, x, y, type, selected = 0):
         self.game = game
         self.x = x
         self.y = y
+        self.s = selected
+        self.type = type
     
     def draw(self):
-        pygame.draw.rect(self.game.window, RED, [[self.x, self.y], [square_lat, square_lat]], 2)
-
-    def setPosition(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def moveUp(self):
-        if self.y - square_lat < y_start:
-            self.setPosition(self.x, grid_height)
-            self.draw()
+        if self.isSelected():    
+            pygame.draw.rect(self.game.window, RED, [[self.x, self.y], [square_lat, square_lat]], 2)
         else:
-            self.setPosition(self, self.x, self.y - square_lat)
-            self.draw()
+            pygame.draw.rect(self.game.window, WHITE, [[self.x, self.y], [square_lat, square_lat]], 2)
+
+    def isSelected(self):
+        return self.s
+
+    def move(self, prev):
+        self.s = 1
+        prev.s = 0
         
-    def moveDown(self):
-        pass
-    def moveRight(self):
-        pass
-    def moveLeft(self):
-        pass
 
 class PipeTube(square):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, game, x, y, direction, selected = 0):
+        super().__init__(game, x, y, selected)
+        self.direction = direction
+
+    def draw(self):
+        tube = pygame.image.load('C:\Users\ana_m\Desktop\PipeGameW\Tube.png')
+        return tube
 
 class PipeCorner(square):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, game, x, y, direction, selected = 0):
+        super().__init__(game, x, y, selected)
+        self.direction = direction
+
 
 class Game:
     def __init__(self):
         self.window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
         pygame.display.set_caption('Pipe Game')
         
-    def draw(self):
+    def gridSquare(self):
+        grid = []
+        y = y_start
+        for i in range(7):
+            aux = []
+            x = x_start
+            for j in range(10):
+                sq = square(self, x, y)
+                aux.append(sq)
+                x += square_lat
+            y += square_lat
+            grid.append(aux)
+        return grid
+
+
+    def draw(self, grid):
         pygame.time.Clock().tick(60)
         pygame.display.update()
 
         # drawing the outline of the grid
         pygame.draw.rect(self.window, WHITE, [[x_start, y_start], [grid_width, grid_height]], 2)
 
-        # drawing the horizontal lines of the grid
-        x = x_start + grid_width
-        y = y_start + square_lat
-        for i in range(7):
-            pygame.draw.line(self.window, WHITE, [x_start, y], [x, y])
-            y += square_lat
-
-        # drawing the vertical lines of the grid
-        x = x_start + square_lat
-        y = y_start + grid_height
-        for i in range(10):
-            pygame.draw.line(self.window, WHITE, [x, y_start], [x, y])
-            x += 80
-        
-       
+        # drawing the lines of the grid
+        for line in grid:
+            for sq in line:
+                sq.draw()
         
 
     def run(self):
+        grid = self.gridSquare()
+        grid[0][0].s = 1 
         while True:
-            self.input()
-            # self.update()
-            self.draw()
-    
-    # def update(self):
-        
+            self.draw(grid)    
+            self.input(grid)        
 
-    def input(self):
-        # drawing the initial square
-        sq = square(self, x_start, y_start)
-        square.draw(sq)
+    def input(self, grid):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                sys.exit()
             if event.type == KEYDOWN:
+                [i,j] = self.selected(grid)
                 if event.key == K_UP:
-                    sq.moveUp()
-        
-            
+                    self.moveUp(grid, i, j)
+                if event.key == K_DOWN:    
+                    self.moveDown(grid, i, j)
+                if event.key == K_RIGHT:
+                    self.moveRight(grid, i, j)
+                if event.key == K_LEFT:
+                    self.moveLeft(grid, i, j)
+    def moveUp(self, grid, i, j):
+        if i == 0:
+            grid[6][j].move(grid[i][j])
+        else:
+            grid[i-1][j].move(grid[i][j])
 
+    def moveDown(self, grid, i, j):
+        if i == 6:
+            grid[0][j].move(grid[i][j])
+        else:
+            grid[i + 1][j].move(grid[i][j])
+
+    def moveLeft(self, grid, i, j):
+        if j == 0:
+            grid[i][9].move(grid[i][j])
+        else:
+            grid[i][j - 1].move(grid[i][j])
+
+    def moveRight(self, grid, i, j):
+        if j == 9:
+            grid[i][0].move(grid[i][j])
+        else:
+            grid[i][j + 1].move(grid[i][j])
+
+
+    def selected(self, grid):
+        for i in range (7):
+            for j in range(10):
+                if grid[i][j].isSelected():
+                    return [i,j]
 
 def main():
     gameInst = Game()
